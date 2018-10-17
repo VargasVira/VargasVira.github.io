9471a50aca2b604c80961ad4fbefbe366e38f641
VargasVira <42445743+VargasVira@users.noreply.github.com>
2018-10-17 22:41:47 +0300

    Додаю фінальне завдання

---
+++ Final_Varhas/script.js
@@ -0,0 +1,13 @@
+
+$('.carousel').carousel({
+	interval:3000,
+	 // keybord:false;
+	
+})﻿
+$('#exampleModal').modal({
+	//  виключаємо закриття вікна кл ESC
+	keyboard:false,
+	// при кліку на модльне вікно мишую його можна закрити, ми це не дозволяємо
+	backdrop:"static"
+	
+})
